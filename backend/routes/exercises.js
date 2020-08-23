9e9c962d4005cd93e27cd332cc39d98abcc5287e
Birajparajuli <37270486+Birajparajuli@users.noreply.github.com>
2020-08-23 20:49:25 +0545

    backend update and delete

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,28 +1,59 @@
 const router = require("express").Router();
-let Exersice = require("../models/exersice.model");
+let Exercise = require("../models/exercise.model");
+const Exersice = require("../models/exercise.model");
 
 router.route("/").get((req, res) => {
-  Exersice.find()
-    .then((exersices) => res.json(exersices))
+  Exercise.find()
+    .then((exercises) => res.json(exercises))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('./add').post((req, res) => {
+router.route("/add").post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
   const date = Date.parse(req.body.date);
 
-  const newExersice = new Exersice({
+  const newExercise = new Exercise({
     username,
     description,
     duration,
     date,
   });
-  newExersice.save()
-    .then(()=>res.json('Exersice Added!'))
-    .catch(err => res.status(400).json('Error: '+ err));
+  newExercise
+    .save()
+    .then(() => res.json("Exercise Added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
+// Find require Data
+router.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Delete Exercise
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise Deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Update Exercise
+router.route('/update/:id').post((req, res)=>{
+  Exercise.findById(req.params.id)
+    .then(exercise=>{
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise.save()
+        .then(()=> res.json('Exercise Updated !'))
+        .catch(err => res.status(400).json('Error: '+ err));
+    })
+    .catch(err => res.status(400).json('Error: '+err));
 });
 
 module.exports = router;
